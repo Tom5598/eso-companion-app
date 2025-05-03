d9e82042edb598df1215e04f8db99ce52f4a6ab4
Tom5598 <karpati.tamas97@gmail.com>
2025-05-03 12:59:27 +0200

    removed spec files

--- src/app/util/timestampt-to-date.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TimestamptToDatePipe } from './timestampt-to-date.pipe';
-
-describe('TimestamptToDatePipe', () => {
-  it('create an instance', () => {
-    const pipe = new TimestamptToDatePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
